d76f0cc9b5e6e35979511c22d1620a5c56383824
gediont <teshomegedion@gmail.com>
2020-08-01 10:17:30 +0300

    gcp firebase conf

---
+++ build/precache-manifest.f8d41fab370168a2892ada67ef823c4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795355b9e0f430d3406593162b572a9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8188537b6df838bb51a7",
+    "url": "/static/css/2.446f2c82.chunk.css"
+  },
+  {
+    "revision": "b4ebd9a33cc0c369304b",
+    "url": "/static/css/main.52bfd372.chunk.css"
+  },
+  {
+    "revision": "8188537b6df838bb51a7",
+    "url": "/static/js/2.a0f56a0a.chunk.js"
+  },
+  {
+    "revision": "b4ebd9a33cc0c369304b",
+    "url": "/static/js/main.c86e2062.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
